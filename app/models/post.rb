1d7e2af157fa5c87a39439464335875d5a8be41f
EikiKanaoka <kakakaeee@gmail.com>
2018-05-17 11:48:29 +0900

    flickr APIをjapan_mapに（jsonは無視）

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
-  validates :title, presence: true
+  # validates :title, presence: true
   validates :content, presence: true
   validates :user_id, presence: true
   validate :picture_size
